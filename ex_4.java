e62df15b61caf09acfb5882b6dec354d6d25e7a5
MariaPogorelova <153062672+MariaPogorelova@users.noreply.github.com>
2025-09-15 15:25:03 +0300

    Add files via upload

---
+++ ex_4.java
@@ -0,0 +1,14 @@
+public class ex_4 {
+    static class A {}
+    static class B extends A {}
+    public static class OverloadInheritance {
+        public void test(A a) { System.out.println("test(A)"); }
+        public void test(B b) { System.out.println("test(B)"); }
+        public static void main(String[] args) {
+            OverloadInheritance o = new OverloadInheritance();
+            A a = new B(); // переменная типа A, объект типа B
+            o.test(a); // ВЫЗОВИТСЯ test(A) — выбор по типу переменной(compile-time)
+            o.test(new B()); // test(B)
+        }
+    }
+}
